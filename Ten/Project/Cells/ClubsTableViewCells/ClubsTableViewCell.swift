c5a7ebcec72a83954595e38897789acaba91be34
amit <amita@inmanage.net>
2019-08-19 15:26:56 +0300

    sprint 12
    create clubs view controller
    code riview by gal

---
+++ Ten/Project/Cells/ClubsTableViewCells/ClubsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClubsTableViewCell.swift
+//  Ten
+//
+//  Created by Amit on 12/08/2019.
+//  Copyright © 2019 Inmanage. All rights reserved.
+//
+
+import UIKit
+
+class TenClubsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
